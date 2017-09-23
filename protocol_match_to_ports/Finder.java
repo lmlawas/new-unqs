6bf733d813dbde20025bd658349f72f1955cc5cd
lmlawas <lmlawas@up.edu.ph>
2017-09-24 06:28:30 +0800

    Create priority text files

--- protocol_match_to_ports/Finder.java
+++ protocol_match_to_ports/Finder.java
@@ -6,22 +6,30 @@ import java.util.Scanner;
 public class Finder{
 
 	public static void main(String[] args){
-		findWord(args[0], new File("port/service-names-port-numbers.csv"));
+		findWord(args[0], args[1], new File("service-names-port-numbers.csv"));
 	}
 
-	public static void findWord(String word, File file) {
+	public static void findWord(String word, String priority, File file) {
 		// int lineNo = 0;		
 		try{
 			Scanner scanner = new Scanner(file);
-			FileWriter fw = new FileWriter(word+".txt", true);
+			FileWriter fw = new FileWriter(priority+".txt", true);
 			while (scanner.hasNextLine()) {
 				// lineNo++;
 			    String nextToken = scanner.nextLine();
 			    Pattern p = Pattern.compile(".*"+word+".*", Pattern.CASE_INSENSITIVE);
 				Matcher m = p.matcher(nextToken);
-			    if (m.matches())
-			    	fw.write(nextToken+"\n\n");
+			    if (m.matches()){
 			    	// System.out.println("line "+ lineNo + ": " + nextToken);
+			    	String[] portProtocols = nextToken.split(",");
+			    	for(int i=0; i<4; i++){
+			    		if(i<=portProtocols.length){
+			    			fw.write(portProtocols[i]+",");
+			    		}
+			    	}
+			    	fw.write("\n");
+			    }			    	
+			    	
 			}
 			fw.close();
 		}catch(Exception e){}		
