06a5e7f345d034cdb44d9361c397450bd8e6d03a
lmlawas <lmlawas@up.edu.ph>
2017-10-01 05:39:22 +0800

    Add basic schedule format

---
+++ program/Schedule.java
@@ -0,0 +1,37 @@
+import java.io.FileWriter;
+import java.util.LinkedList;
+
+public class Schedule {
+
+	/* Constants */
+	final public static int FIFO = 0;
+	final public static int PQ = 1;
+	final public static int WFQ = 2;
+
+
+	public int process(int schedule_type) {
+
+		if (schedule_type == FIFO) {
+			firstInFirstOut();
+		} else if (schedule_type == PQ) {
+			priorityQueue();
+		} else if (schedule_type == WFQ) {
+			weightedFairQueue();
+		} else {
+			return -1;
+		}
+		return 1;
+	}
+
+	public void firstInFirstOut() {
+
+	}
+
+	public void priorityQueue() {
+
+	}
+
+	public void weightedFairQueue() {
+
+	}
+}
