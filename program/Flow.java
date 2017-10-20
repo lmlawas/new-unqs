217d2a7e9eaa5dac78bf0306d0ecc916e1ff834c
lmlawas <lmlawas@up.edu.ph>
2017-10-20 14:17:01 +0800

    Correct convertToPackets()

--- program/Flow.java
+++ program/Flow.java
@@ -24,12 +24,13 @@ public class Flow {
 		LinkedList<Packet> packets = new LinkedList<Packet>();
 		int i, quotient = size / no_of_packets, remainder = size % no_of_packets;
 
-		for (i = 0; i < no_of_packets; i++) {
+		for (i = 0; i < no_of_packets; i++) {			
 			if (size % no_of_packets > 0 && i == no_of_packets - 1) {
-				packets.add(new Packet(quotient + remainder, protocol, start_time, schedule_type));
+				packets.add(new Packet(size + remainder, protocol, start_time, schedule_type));
 			} else {
 				packets.add(new Packet(quotient, protocol, start_time, schedule_type));
 			}
+			size = size - quotient;
 		}
 
 		return packets;
